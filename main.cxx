5480a9182109dbcb45526e7e0927606a56df5713
Marcus O'Riordan <marcus.oriordan4@gmail.com>
2025-10-02 16:40:18 +0200

    Task1 header and source file, as well as main to test

--- main.cxx
+++ main.cxx
@@ -6,8 +6,19 @@
 #include "as1.hpp"
 #include <iostream>
 
-int main() { 
-  // Example for as1.0
-  homework::printHello();
-}
+int main() {
+    using namespace homework;
 
+    printHello();
+   
+    int a = 5;
+    AddOneRef(a);
+    std::cout << "AddOneRef result: " << a << "\n";
+
+    std::cout << "isOdd(5): " << isOdd(20) << "\n";
+    std::cout << "floatToInt(3.7): " << floatToInt(3.7f) << "\n";
+    std::cout << "factorial(5): " << factorial(5) << "\n";
+    std::cout << "factorial(-2): " << factorial(-2) << "\n";
+
+    return 0;
+}
