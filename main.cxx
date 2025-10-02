83b4900c897879ebf6bfd2000e742dc072d8250d
Koshin Puri <koshin.puri.0833@student.lu.se>
2025-10-02 16:40:01 +0200

    Task 1 submission

--- main.cxx
+++ main.cxx
@@ -1,13 +1,19 @@
-/** Use this main to test exercises. See the example with 'as1.0' below. 
- *  You can add all the exercise tests inside the same main.
- *  Don't forget to add includes properly.
- * */
-
 #include "as1.hpp"
 #include <iostream>
 
-int main() { 
-  // Example for as1.0
-  homework::printHello();
-}
+int main() {
+    using namespace homework;
 
+    printHello(); 
+    
+    int a = 5;
+    AddOneRef(a);
+    std::cout << "AddOneRef result: " << a << "\n";
+
+    std::cout << "isOdd(5): " << isOdd(5) << "\n";
+    std::cout << "floatToInt(3.7): " << floatToInt(3.7f) << "\n";
+    std::cout << "factorial(5): " << factorial(5) << "\n";
+    std::cout << "factorial(-2): " << factorial(-2) << "\n";
+
+    return 0;
+}
