580e3a9e41ac9f02e5839d6723a49dcb78ec4781
Marie-Philine <ma3648he-s@student.lu.se>
2025-10-04 18:27:01 +0200

    first steps of homework

--- main.cxx
+++ main.cxx
@@ -5,9 +5,28 @@
 
 #include "as1.hpp"
 #include <iostream>
+using namespace std;
 
 int main() { 
   // Example for as1.0
   homework::printHello();
+
+  // as 1.1
+  int variable = 4;
+  cout << "Before: " << variable << endl;
+  homework::AddOneRef(variable);
+  cout << "After: " << variable << endl;
+
+  // as 1.2
+  cout << "The value of the variable is " << variable << ". Is this an odd number?\n" ;
+  if (homework::isOdd(variable)) {std::cout << "true \n";}
+  else {std::cout << "false \n";}
+  
+  homework::AddOneRef(variable);
+  cout << "The value of the variable is " << variable << ". Is this an odd number?\n" ;
+  if (homework::isOdd(variable)) {std::cout << "true \n";}
+  else {std::cout << "false \n";}
+
+  
 }
 
--- src/as1.cxx
+++ src/as1.cxx
@@ -4,9 +4,10 @@ namespace homework {
 
 void printHello() { std::cout << "Hello, World!" << std::endl; }
 
-void AddOneRef(int &x) { return; }
+void AddOneRef(int &x) { x += 1; } // adds 1 to some variable x defined in main
 
-bool isOdd(int x) { return false; }
+bool isOdd(int x) { 
+    return x % 2 != 0;}
 
 int floatToInt(float x) { return 0; }
 
