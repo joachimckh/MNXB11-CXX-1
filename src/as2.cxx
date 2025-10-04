aa427464a1fffbef66b354dd8c5976002fb76957
Ana Rodrigues <an6158fe-s@student.lu.se>
2025-10-04 22:10:28 +0200

    final code

--- src/as2.cxx
+++ src/as2.cxx
@@ -2,10 +2,30 @@
 
 namespace homework {
 
-// implement Foo methods here
-int Foo::bar() { 
-  return -1; 
+
+int Foo::bar() {
+    return 42;
+}
+
+float Foo::baz() {
+    x = 2.71f;
+    return 3.14f;
+}
+
+std::vector<double> Foo::quux() {
+    return {1.0, 2.0, 3.0};
 }
 
 
-} // namespace homework
+fVector2D operator+(const fVector2D& a, const fVector2D& b) {
+    return fVector2D(a.x_ + b.x_, a.y_ + b.y_);
+}
+
+
+bool fVector2D::operator==(const fVector2D& other) const {
+    return x_ == other.x_ && y_ == other.y_;
+}
+
+} 
+
+
--- src/as3.cxx
+++ src/as3.cxx
@@ -1,9 +1,18 @@
 #include "as3.hpp"
+using namespace std;
 
 namespace homework {
 
+Fruit::Fruit(const string& name, Color c) : name_(name), color_(c) { }
+Fruit::~Fruit() = default;
+
+string Fruit::getName() const { return name_; }
+Color Fruit::getColor() const { return color_; }
+
+Apple::Apple(Color c) : Fruit("apple", c) { }
+string Apple::getTaste() const { return "sweet"; }
+
+}
 
 
-
-} // namespace homework
 
