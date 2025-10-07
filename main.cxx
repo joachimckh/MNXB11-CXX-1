bd4bd8a037c7def7b140093efb3879aec90b3f8e
Dominique de Boer <dominiquedeboer@yahoo.nl>
2025-10-07 12:36:36 +0200

    as1 done

--- main.cxx
+++ main.cxx
@@ -3,11 +3,39 @@
  *  Don't forget to add includes properly.
  * */
 
-#include "as1.hpp"
+#include "include/as1.hpp"
 #include <iostream>
 
-int main() { 
-  // Example for as1.0
-  homework::printHello();
-}
+// int main() { 
+//   // Example for as1.0
+//   homework::printHello();
+//   return 0;
+// }
 
+// int main() {
+//   int a = 2;
+//   homework::AddOneRef(a);
+//   std::cout << "a = " << a << std::endl;
+//   return 0;
+// }
+
+// int main() {
+//   int a = 7;
+//   if (homework::isOdd(a) == false) 
+//     std::cout << a << " is even" << std::endl;
+//   else 
+//     std::cout << a << " is odd" << std::endl;
+//   return 0;
+// }
+
+// int main() {
+//   float x = 5.99;
+//   std::cout << "the float " << x << " is integer " << homework::floatToInt(x) << std::endl;
+//   return 0;
+// }
+
+int main() {
+  int n = 9;
+    std::cout << "the factorial of " << n << " is " << homework::factorial(n) << std::endl;
+    return 0;
+}
