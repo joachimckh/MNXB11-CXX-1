5ca251cdee2b72933c00b027f10030a6e5a6add2
Koshin Puri <koshin.puri.0833@student.lu.se>
2025-10-07 19:41:13 +0200

    Task 3 Submission

--- main.cxx
+++ main.cxx
@@ -1,31 +1,13 @@
-#include "as2.hpp"
+#include "as3.hpp"
 #include <iostream>
 
 using namespace homework;
 
 int main() {
-    std::cout << "=== Task 2 Test ===\n\n";
-
-    // Test Foo struct
-    Foo f;
-    std::cout << "Foo::bar() = " << f.bar() << "\n";
-
-    std::cout << "Foo::baz() = " << f.baz() << ", x = " << f.x << "\n";
-
-    auto v = f.quux();
-    std::cout << "Foo::quux() = ";
-    for (auto d : v) std::cout << d << " ";
-    std::cout << "\n\n";
-
-    // Test fVector2D operators
-    fVector2D a(1, 2), b(3, 4);
-    auto c = a + b;
-    std::cout << "fVector2D a+b == (4,6)? " 
-              << ((c == fVector2D(4,6)) ? "true" : "false") << "\n";
-
-    fVector2D d(0,0);
-    std::cout << "fVector2D d == (0,0)? " 
-              << ((d == fVector2D(0,0)) ? "true" : "false") << "\n";
+    Apple a(Color::red);
+    std::cout << a.getName() << " is color "
+              << static_cast<int>(a.getColor())
+              << " and tastes " << a.getTaste() << "\n";
 
     return 0;
 }
