8a3bdf2468d38e9cef863fb6d9e2b2fe02657774
“German <ge2465lu-s@student.lu.se>
2025-10-07 17:43:32 +0200

    solution of the task 3 and small fixes to the task 2

--- include/as2.hpp
+++ include/as2.hpp
@@ -26,7 +26,7 @@ struct Foo {
 class fVector2D {
 public:
   fVector2D() = default;
-  fVector2D(float x, float y) : x_(x), y_(y) {};
+  fVector2D(float x, float y) : x_(x), y_(y) {}
   bool operator==(const fVector2D& other) const;
   friend fVector2D operator+(const fVector2D lhs, const fVector2D rhs);
 
--- src/as3.cxx
+++ src/as3.cxx
@@ -2,7 +2,17 @@
 
 namespace homework {
 
+std::string Fruit::getName() const{
+    return name_;
+}
 
+Color Fruit::getColor() const{
+    return color_;
+}
+
+virtual std::string Fruit::getTaste() const{
+
+}
 
 
 } // namespace homework
