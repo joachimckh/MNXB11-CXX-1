e78556ba72393237d0412db30bb909131311583c
Vishvam <vishvamkpatel18@gmail.com>
2025-10-07 13:04:24 +0200

    Added manual test in main.cxx to show code execution

--- main.cxx
+++ main.cxx
@@ -1,13 +1,35 @@
-/** Use this main to test exercises. See the example with 'as1.0' below. 
- *  You can add all the exercise tests inside the same main.
- *  Don't forget to add includes properly.
- * */
-
-#include "as1.hpp"
 #include <iostream>
+#include "as1.hpp"
+#include "as2.hpp"
+#include "as3.hpp"
 
-int main() { 
-  // Example for as1.0
-  homework::printHello();
+int main() {
+    using namespace homework;
+
+    // Assignment 1
+    int a = 1;
+    AddOneRef(a);
+    std::cout << "AddOneRef test: " << a << " (should be 2)" << std::endl;
+
+    std::cout << "isOdd(3) = " << isOdd(3) << " (should be 1)" << std::endl;
+
+    std::cout << "floatToInt(3.14) = " << floatToInt(3.14f) << " (should be 3)" << std::endl;
+
+    std::cout << "factorial(5) = " << factorial(5) << " (should be 120)" << std::endl;
+
+    // Assignment 2
+    Foo f;
+    std::cout << "Foo::bar() = " << f.bar() << std::endl;
+    std::cout << "Foo::baz() = " << f.baz() << std::endl;
+
+    fVector2D v1(1.0f, 2.0f);
+    fVector2D v2(3.0f, 4.0f);
+    fVector2D v3 = v1 + v2;
+    std::cout << "fVector2D + operator: (" << v3.x_ << ", " << v3.y_ << ")" << std::endl;
+
+    // Assignment 3
+    Apple apple(Color::red);
+    std::cout << "Apple taste: " << apple.getTaste() << std::endl;
+
+    return 0;
 }
-
