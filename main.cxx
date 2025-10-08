3a946a58eea11b3cd50d52bb2d61d830be35e372
Ossian Malmborg <os2287ma-s@student.lu.se>
2025-10-08 15:40:14 +0200

    First assignment should work

--- main.cxx
+++ main.cxx
@@ -9,5 +9,12 @@
 int main() { 
   // Example for as1.0
   homework::printHello();
+  int a = 1;
+  std::cout << "a = 1 " << std::endl;
+  homework::AddOneRef(a);
+  std::cout << "and a is now " << a << std::endl;
+  std::cout << homework::isOdd(3) << " is the output and the answer is 1" << std::endl;
+  std::cout << homework::floatToInt(3.14) << " is the output and the answer is 3" << std::endl;
+  std::cout << homework::factorial(6) << " is the output and the answer is 720" << std::endl;
 }
 
--- src/as1.cxx
+++ src/as1.cxx
@@ -4,12 +4,27 @@ namespace homework {
 
 void printHello() { std::cout << "Hello, World!" << std::endl; }
 
-void AddOneRef(int &x) { return; }
+void AddOneRef(int &x) {++x;}
 
-bool isOdd(int x) { return false; }
+bool isOdd(int x) { 
+    return x % 2 != 0;
+}
 
-int floatToInt(float x) { return 0; }
+int floatToInt(float x) { 
+    return static_cast<int>(x);
+}
 
-int factorial(int n) { return 0; }
+int factorial(int n) { 
+    int x;
+    if (n < 0) {
+    x = -1;
+    } else {
+        x = 1;
+        for (; n > 0; n-=1) {
+            x *= n;
+        }
+    }  
+    return x;
+}
 
 }; // namespace homework
