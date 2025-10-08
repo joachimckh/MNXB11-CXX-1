d25b7345a196a62298e276005b4daafc871af92f
Axel Cerne <ax4888ce-s@student.lu.se>
2025-10-08 16:22:28 +0200

    assignment 1 done, added .vscode/ to gitignore

--- main.cxx
+++ main.cxx
@@ -7,7 +7,53 @@
 #include <iostream>
 
 int main() { 
+
+  /* ---------------asignment 1-------------------*/
+
   // Example for as1.0
   homework::printHello();
+
+  // test AddOneRef()
+  int x;
+  homework::AddOneRef(x); // seems to work, but can't print since void(?)
+
+  // test isOdd()
+  int y = 3;
+  int z = 4;
+  int f = -3;
+
+  std::cout << homework::isOdd(y) << std::endl; // prints 1 = true 
+  std::cout << homework::isOdd(z) << std::endl; // prints 0 = false
+  std::cout << homework::isOdd(f) << std::endl; // prints 1 = true 
+
+
+  // test floatToInt()
+  float a = 0.10;
+  float b = 3.77;
+  std::cout << homework::floatToInt(a) << std::endl; // prints 0
+  std::cout << homework::floatToInt(b) << std::endl; // prints 3
+
+
+  // test factorial()
+  int n = 3;
+  int nn = 4;
+  int nnn = 0;
+  int nnnn = -10;
+
+  
+  std::cout << homework::factorial(n) << std::endl;  // prints 6 = 3*2*1
+  std::cout << homework::factorial(nn) << std::endl; // prints 24 = 4*3*2*1
+  std::cout << homework::factorial(nnn) << std::endl; // prints 1
+  std::cout << homework::factorial(nnnn) << std::endl; // prints -1
+  
+  
+
+
+  /*-------------assignment 2-------------------*/
+
+
+
+
+
 }
 
--- src/as1.cxx
+++ src/as1.cxx
@@ -2,14 +2,40 @@
 
 namespace homework {
 
-void printHello() { std::cout << "Hello, World!" << std::endl; }
+void printHello() 
+{ 
+    std::cout << "Hello, World!" << std::endl; 
+}
 
-void AddOneRef(int &x) { return; }
+void AddOneRef(int &x) 
+{ 
+    x += 1; 
+}
 
-bool isOdd(int x) { return false; }
+bool isOdd(int x) 
+{ 
+    bool b = true;
+    
+    if (x%2 == 0)   // if x is even b <-- false
+        b = false;
 
-int floatToInt(float x) { return 0; }
+    return b; // return true if not x = even (lol)
+}
 
-int factorial(int n) { return 0; }
+int floatToInt(float x) 
+{ 
+    return static_cast<int>(x);
+}
+
+int factorial(int n) 
+{ 
+    if (n < 0)  // take care of negatives
+        return -1;
+
+    if (n == 0) // take care of 0 because math
+        return 1;
+
+    return n*factorial(n-1); // recursive return
+}
 
 }; // namespace homework
