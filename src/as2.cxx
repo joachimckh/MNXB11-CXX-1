15ce9f9b6d09450f99d1ee45a937820cc18f4fb8
Lasse <lasseulricht@gmail.com>
2025-10-10 11:01:12 +0200

    implemented methods for as2

--- src/as2.cxx
+++ src/as2.cxx
@@ -4,8 +4,17 @@ namespace homework {
 
 // implement Foo methods here
 int Foo::bar() { 
-  return -1; 
+  return 42; 
 }
+float Foo::baz(){
+  x = 2.71f;
+  return 3.14f;
+}
+
+std::vector<double> quux(){
+  return{1.0,2.0,3.0};
+}
+
 
 
 } // namespace homework
