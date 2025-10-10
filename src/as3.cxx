099b848474574262b796adcde16ce4600d76f3cf
Victor Hallberg <victorhallberg12@gmail.com>
2025-10-10 17:17:58 +0200

    homework yahoo

--- src/as3.cxx
+++ src/as3.cxx
@@ -1,9 +1,20 @@
 #include "as3.hpp"
 
 namespace homework {
+Fruit::Fruit(const std::string &name, Color color) : name_(name), color_(color) {}
 
+std::string Fruit::getName() const {
+    return name_;
+}
 
+Color Fruit::getColor() const {
+    return color_;
+}
+Apple::Apple(Color color) : Fruit("apple", color) {}
 
+std::string Apple::getTaste() const {
+    return "sweet";
+}
 
 } // namespace homework
 
