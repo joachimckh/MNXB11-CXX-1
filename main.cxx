a8756dc307d8beafc194435ccb9ec5556b77d088
Lasse <lasseulricht@gmail.com>
2025-10-10 09:47:29 +0200

    implemented minor changes as well as testing for as1

--- main.cxx
+++ main.cxx
@@ -6,8 +6,25 @@
 #include "as1.hpp"
 #include <iostream>
 
+
 int main() { 
   // Example for as1.0
   homework::printHello();
+
+  //testing as1.cxx AddOneRef
+  int x = 1;
+  homework::AddOneRef(x);
+  std::cout << x << std::endl;
+  //testing as1.cxx  is Odd
+  std::cout << homework::isOdd(3) << std::endl;
+  std::cout <<homework::isOdd(4)<< std::endl;
+  //testing as1.cxx floatToInt
+  std::cout << homework::floatToInt(2.71) << std::endl;
+  std::cout << homework::floatToInt(-2.71) << std::endl;
+  //testing as1.cxx 
+  std::cout << homework::factorial(7) << std::endl;
+  std::cout << homework::factorial(0) << std::endl;
+  std::cout << homework::factorial(-4)<< std::endl;
+
 }
 
--- src/as1.cxx
+++ src/as1.cxx
@@ -23,17 +23,22 @@ int floatToInt(float x) {
     }
 
 int factorial(int n) { 
-    if(n<0){return -1;}
+    if(n<0){
+        return -1;
+    }
     else if (n == 0)
     {
         return 1;
     }
     else{
     int result;
+        int a = 1;
         for(int i = 1; i<=n; i++){
-            result *=i;
+
+            a *=i;
+            
         }
-        return result;
+        return a;
     }
  }
 
