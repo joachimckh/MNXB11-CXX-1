d2c8b5466af0144eeaae0fe4498437c5c414625d
Nellie Gustafsson <ne8378gu-s@student.lu.se>
2025-10-10 15:34:43 +0200

    Finished homework and all tests pass

--- src/as1.cxx
+++ src/as1.cxx
@@ -2,14 +2,32 @@
 
 namespace homework {
 
-void printHello() { std::cout << "Hello, World!" << std::endl; }
+void printHello() { 
+    std::cout << "Hello, World!" << std::endl;  //Prints Hello World
+}
 
-void AddOneRef(int &x) { return; }
+int AddOneRef(int &x) { //Adds one to x
+    x += 1;
+    return x;
+}
 
-bool isOdd(int x) { return false; }
+bool isOdd(int x) { //Checks if x is odd or ever
+    if (x%2 == 0){
+        return false;
+    }
+    else {
+        return true;
+    }
+}
 
-int floatToInt(float x) { return 0; }
+int floatToInt(float y) { //Casts a float to an int
+    return static_cast<int>(y); 
+}
 
-int factorial(int n) { return 0; }
+int factorial(int n) { //Returns the factorial of n
+    if (n<0) return -1;
+    if (n == 0) return 1;
+    return n * factorial(n-1);
+}
 
 }; // namespace homework
