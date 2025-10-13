f5c4d70c684fab65fa89e22a37dc738dcf0bfdf9
Akash Arjun <arjun@cosmos-pkf.int>
2025-10-13 04:12:29 +0200

    task2 and task3 done, but should be checked once

--- include/as2.hpp
+++ include/as2.hpp
@@ -26,6 +26,8 @@ class fVector2D {
 public:
   fVector2D() = default;
   fVector2D(float x, float y) : x_(x), y_(y) {}
+  bool operator ==(const fVector2D& other)const;
+  friend fVector2D operator+(const fVector2D& a, const fVector2D& b);
 
 private:
   float x_;
--- include/as3.hpp
+++ include/as3.hpp
@@ -9,15 +9,33 @@ namespace homework {
 // fruits 
 // (a) TO DO: Implement your type (Hint: enum class). Name it "Color" and add three colors:
 // red, green, yellow
-
+enum class Color{red,green,yellow};
 // (b) TO DO: Implement a class called "Fruit" that has a constructor taking a
 // string and a "Color" and two methods: "getName" and "getColor" Also implement
 // a pure virtual method "getTaste" that returns a string
+class Fruit{
+    protected:
+    std::string name;
+    Color color;
+
+    public:
+    Fruit(const std::string& name, Color color);
+
+    std::string getName() const;
+    Color getColor() const;
+
+    virtual std::string getTaste() const = 0;
+    virtual ~Fruit() = default;
+};
 
 // (c) TO DO: Implement a class called "Apple" that inherits from "Fruit"
 // implement the constructor and the "getTaste" method
 // The taste of an apple is "sweet"
 // The constructor should take a "Color" as argument and pass the name "apple"
 // to the base class constructor
-
+class Apple : public Fruit {
+public:
+    Apple(Color color);
+    std::string getTaste() const override;
+};
 } // namespace homework
