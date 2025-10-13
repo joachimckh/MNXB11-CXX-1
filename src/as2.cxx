fa58ff83e4981bc8a5038e5851ba943ca5ac8fa8
Filip Fast <filip.fast04@gmail.com>
2025-10-13 10:53:19 +0200

    attempt eight

--- src/as2.cxx
+++ src/as2.cxx
@@ -1,4 +1,6 @@
 #include "as2.hpp"
+#include <vector>
+#include <iostream> 
 
 namespace homework {
 
@@ -12,15 +14,35 @@ float Foo::baz() {
   return 3.14;
 }
 
-std::vector<double> quux() {
+std::vector<double> Foo::quux() {
   return {1.0, 2.0, 3.0};
 }
 
+//default generates default constructor
+
+//float fVector2Da() {}
+  //return fVector2D ();
 
 
+//float fVector2Db() {}
 
-       
 
+//float test() {
+
+//fVector2D a(1.1, 2.0); 
+
+//fVector2D b(1.2, 2.1); 
+
+//fVector2D c = a + b; 
+
+//c.print(); if need be
+
+//if (a == b) { std::cout << "both elements are equal" << std::endl; 
+//} else { std::cout << "elements are not equal" << std::endl; }
+
+//return 0;
+
+//}
 
 
 } // namespace homework
--- src/as3.cxx
+++ src/as3.cxx
@@ -1,6 +1,27 @@
 #include "as3.hpp"
+#include <string>
 
 namespace homework {
+
+enum class Color {Red, Green, Yellow};
+
+//class Fruit {
+
+//private: 
+
+//std::string name;
+
+//Color;
+
+//public:
+
+//Fruit (std::string& Name, Color):  {}
+
+//std::string getName() { return name; }
+
+//std::string getColor() {}
+
+
 
 
 
