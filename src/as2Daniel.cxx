949fa6498c3964667141059c00c6e0632b7d11f9
Daniel Stålhammar <danst@cosmos-pkf.int>
2025-10-15 17:33:38 +0200

    Kanske jag lyckas skicka dem nu

---
+++ src/as2Daniel.cxx
@@ -0,0 +1,32 @@
+#include "as2.hpp"
+
+namespace homework {
+
+// implement Foo methods here
+
+int bar(){
+  return 42;
+}
+
+int baz(int ,x){
+  x = 2.71
+  return 3.14;
+
+}
+
+void quux(v){
+  (if len(v)==3){nyv=2*v return nyv}
+  else cout << "Hadde inte tre ellement";
+}
+
+int Foo::bar() { 
+  return -1; 
+}
+
+
+int operator(vector1,vector2){
+  (if vector1==vector2){return true};
+  else
+}
+
+} // namespace homework
